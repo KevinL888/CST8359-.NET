612b1bb2dee64395e294d219a24c29d5f18ddc6f
Kevin Lai <kevinlai838@gmail.com>
2019-04-28 23:24:36 -0400

    Add files via upload

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
