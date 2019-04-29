17caf4e2c344c31b1566890a55cc06111a5a3393
Kevin Lai <kevinlai838@gmail.com>
2019-04-28 23:34:22 -0400

    Add files via upload

---
+++ Lab4/Controllers/Home.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab4.Controllers
+{
+    public class Home : Controller
+    {
+
+        private MoviesContext _moviesContext;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public Home(MoviesContext context)
+        {
+            _moviesContext = context;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_moviesContext.Movie.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie(Movie movie)
+        {          
+                _moviesContext.Movie.Add(movie);
+                _moviesContext.SaveChanges();
+
+                return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteMovie(int id)
+        {
+            var movieToDelete = (from m in _moviesContext.Movie where m.MovieId == id select m).FirstOrDefault();
+            _moviesContext.Movie.Remove(movieToDelete);
+            _moviesContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditMovie(int id)
+        {
+            var movieToUpdate = (from m in _moviesContext.Movie where m.MovieId == id select m).FirstOrDefault();
+
+            return View(movieToUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult ModifyMovie(Movie movie)
+        {
+            var id = Convert.ToInt32(Request.Form["MovieId"]);
+
+            var movieToUpdate = (from m in _moviesContext.Movie where m.MovieId == id select m).FirstOrDefault();
+
+
+            try
+            {
+                movieToUpdate.Title = movie.Title;
+
+                movieToUpdate.SubTitle = movie.SubTitle;
+
+                movieToUpdate.Description = movie.Description;
+            
+                movieToUpdate.Year = movie.Year;
+
+                movieToUpdate.Rating = movie.Rating;
+
+                if (movieToUpdate.Rating > 5 || movieToUpdate.Rating < 1)
+                    throw new Exception();
+
+                _moviesContext.SaveChanges();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Lab4/Models/MoviesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
