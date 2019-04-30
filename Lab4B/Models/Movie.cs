a52bec6fcc1cb07f7c4577562d1e6df1415d77e5
Kevin Lai <kevinlai838@gmail.com>
2019-04-30 12:05:01 -0400

    Add files via upload

---
+++ Lab4B/Models/Movie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Movie
+    {
+        public int MovieId
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string SubTitle
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Year
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [Range(1,5)]
+        public int Rating
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
